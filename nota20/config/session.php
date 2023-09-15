2fe20e7b9555b9db916bdc30dacce09e679e4a51
Manguena <jordaomanguena@yahoo.com>
2023-09-15 23:02:15 +0200

    Chang in duration of session
    Change the duration of the session from 1 to 5 minutes

--- nota20/config/session.php
+++ nota20/config/session.php
@@ -30,7 +30,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 1),
+    'lifetime' => env('SESSION_LIFETIME', 5),
     //'lifetime' => env('SESSION_LIFETIME', 120),->original which then i changed
 
     'expire_on_close' => true,//the session exprires if the browser is closed
